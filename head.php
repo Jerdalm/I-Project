6f4010796f591302e4b8fc4ac26dcfcebf8bb85a
Kev4ever <kevin.van.schaijk01@gmail.com>
2018-04-24 16:10:18 +0200

    Slider/backgroundheader

--- head.php
+++ head.php
@@ -1,5 +1,5 @@
 <?php
-
+require_once 'mechanic/functions.php'; 
 $head = <<<HEAD
 	
 	<DOCTYPE html>
--- vendor/bootstrap/js/popup.header.js
+++ vendor/bootstrap/js/popup.header.js
@@ -2,3 +2,26 @@ $(window).scroll(function() {
     var scrolled = $(this).scrollTop() > 1;
     $("nav.navbar").css('background-color', scrolled ? 'rgba(0, 0, 0, 0.4)' : "rgba(0, 0, 0, 0)");
 });
+
+$("#myCarousel").on("slide.bs.carousel", function(e) {
+  var $e = $(e.relatedTarget);
+  var idx = $e.index();
+  var itemsPerSlide = 3;
+  var totalItems = $(".carousel-item").length;
+
+  if (idx >= totalItems - (itemsPerSlide - 1)) {
+    var it = itemsPerSlide - (totalItems - idx);
+    for (var i = 0; i < it; i++) {
+      // append slides to end
+      if (e.direction == "left") {
+        $(".carousel-item")
+          .eq(i)
+          .appendTo(".carousel-inner");
+      } else {
+        $(".carousel-item")
+          .eq(0)
+          .appendTo($(this).find(".carousel-inner"));
+      }
+    }
+  }
+});
