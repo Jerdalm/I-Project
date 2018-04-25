7fc975123084ea2acb2b870511d73304cc009dcc
eduardbakouev <edi_bakouev@hotmail.nl>
2018-04-25 12:27:19 +0200

    head Met mijn stylesheet - edje

--- head.php
+++ head.php
@@ -15,6 +15,7 @@ $head = <<<HEAD
 		<link rel="stylesheet" type="text/css" href="../I-Project/vendor/bootstrap/css/bootstrap-reboot.css">
 		<link rel="stylesheet" type="text/css" href="../I-Project/css/jonathan.css">
 		<link rel="stylesheet" type="text/css" href="../I-Project/css/style.css">
+		<link rel="stylesheet" type="text/css" href="../I-Project/css/stylesheet_Edje.css">
 		<link href="css/styles_kevin.css" rel="stylesheet">
 		 <script src="vendor/jquery/jquery.min.js"></script>
 		<title>EenmaalAndermaal</title>
---
+++ userDetails.php
@@ -0,0 +1,58 @@
+<?php require_once 'header.php'; ?>
+
+<body>
+<main>
+<div class="container">
+   
+
+
+        <div class="row row-left">
+            <div class="plaatje">
+                <img src="img/geit.jpg">
+            </div>
+        <div class="row row-right">
+        <table class="table"> 
+            <thead>
+                <tr>
+                     <th scope="col">Gebruikersnaam</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <th scope="row">Land</th>
+                        <td> test`1</td>
+                </tr>
+                <tr>
+                    <th scope="row">Woonplaats</th>
+                </tr>
+                <tr>
+                    <th scope="row">E-mailadres</th>
+                </tr>
+                <tr>
+                    <th scope="row">Voornaam</th>
+                </tr>
+                 <tr>
+                    <th scope="row">Achternaam</th>
+                </tr>
+                 <tr>
+                    <th scope="row">Telefoonnummer</th>
+                </tr>
+                 <tr>
+                    <th scope="row">Postcode</th>
+                </tr>
+                  <tr>
+                    <th scope="row">Wachtwoord</th>
+                <td> <a href=""> <b><i>Wachtwoord wijzigen</i></b> </a></td>
+                </tr>
+            </tbody>    
+        </table> 
+
+        </div>
+    </div>
+
+
+
+
+</main>
+<?php require_once 'footer.php'; ?>
+
