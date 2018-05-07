13631c7e7a198082f132a003ab2529e85d3aea16
JonathanDeBelg <j.dionant@gmail.com>
2018-05-07 15:14:34 +0200

    eduards code mooi gemaakt

--- wachtwoord-vergeten.php
+++ wachtwoord-vergeten.php
@@ -4,32 +4,12 @@ $_SESSION['mailAdres'] = '';
 if (isset($_POST['check'])) {
 	$_SESSION['mailAdres'] = $_POST['mailadres'];
 	$emailAdres = $_POST['mailadres'];
-
 }
-
-?>
-<main>
-
-
-
- <?php 
-
- 
-
 $emailParameters = array(':mailadres' => "$emailAdres");
-
-$gebruiker = handlequery("SELECT *
- FROM Gebruiker JOIN Vraag
- on Gebruiker.vraag = Vraag.vraagnummer
- where mailadres = :mailadres 
- and
- Gebruiker.vraag = Vraag.vraagnummer", $emailParameters);
-
+$gebruiker = handlequery("SELECT * FROM Gebruiker JOIN Vraag ON Gebruiker.vraag = Vraag.vraagnummer WHERE mailadres = :mailadres 
+ AND Gebruiker.vraag = Vraag.vraagnummer", $emailParameters);
 
 // $_SESSION['mailadres'] = $gebruiker['mailadres']; 
-
-
-
 
 $email = $emailAdres;
 $subject = 'Wachtwoord wijzigen';
@@ -39,69 +19,37 @@ $randomPassword = createRandomPassword();
 $messageCode = $message . $randomPassword;
 ?>
 
+<main>
+    <form method="POST" class="form-group">
+        <label for="E-mailadres">Voer hier uw E-mailadres in: </label>
+            <input type="text" name="mailadres" class="form-control" id="mailadres" placeholder="E-mailadres" value="<?= $_SESSION['mailAdres'] ?>" >
+            <input class="cta-orange btn" type="submit" name="check" value="Controlleer">
 
+            <?php 
+                if( isset($_POST['mailadres']) && count($gebruiker) == 1 ) { 
+            ?>
+                    <form method="POST" class="form-group">
+                        <label for="testvoorvraag">  <?= $gebruiker[0]['vraag']?> </label>
+                            <input type="text" name="antwoord" class="form-control" id="testAntwoordvakje" placeholder="Antwoord">
+                            <input class="cta-orange btn" type="submit" name="verzenden" value="Verzenden">
+                    </form>
+            <?php 
+                }  
+                if (isset ($_POST['verzenden'])){
+                    $antwoordtekst = $_POST['antwoord'];
+                    $emailAdres = $_POST['mailadres'];
+                    $answerParameters = array(':antwoord' => "$antwoordtekst", ':mailadres' => "$emailAdres" );
+                    $antwoord = handlequery("SELECT antwoordtekst FROM Gebruiker WHERE antwoordtekst = :antwoord AND mailadres = :mailadres" , $answerParameters);
 
+                    if (count($antwoord) == 1) {
+                        sendMail($email,$subject,$messageCode);
+                        handlequery("UPDATE Gebruiker SET wachtwoord = '$randomPassword' WHERE mailadres = '$emailAdres' ");
 
-
-
-
-
-
-
-
-
-<form method="POST" class="form-group">
-    <label for="E-mailadres"> Voer hier uw E-mailadres in: </label>
-        <input type="text" name="mailadres" class="form-control" id="mailadres" placeholder="E-mailadres" value="<?php echo $_SESSION['mailAdres'] ?>" >
-        <input class="cta-orange btn" type="submit" name="check" value="Controlleer">
-
- 
-
-<?php if( isset($_POST['mailadres']) && count($gebruiker) == 1 ) { ?>
-
-<form method="POST" class="form-group">
-    <label for="testvoorvraag">  <?php echo $gebruiker[0]['vraag']?> </label>
-        <input type="text" name="antwoord" class="form-control" id="testAntwoordvakje" placeholder="Antwoord">
-        <input class="cta-orange btn" type="submit" name="verzenden" value="Verzenden">
-</form>
-
-<?php }  ?>
-
-
-
-
- <?php     if (isset ($_POST['verzenden'])){
-
-$antwoordtekst = $_POST['antwoord'];
-$emailAdres = $_POST['mailadres'];
-
-$answerParameters = array(':antwoord' => "$antwoordtekst" , 
-                          ':mailadres' => "$emailAdres" );
-
-$antwoord = handlequery("SELECT antwoordtekst
-                         FROM Gebruiker
-                         WHERE antwoordtekst = :antwoord
-                         AND mailadres = :mailadres" , $answerParameters);
-
-
-
-if (count($antwoord) == 1) {
-
-    sendMail($email,$subject,$messageCode);
-
-handlequery("UPDATE Gebruiker SET wachtwoord = '$randomPassword' WHERE mailadres = '$emailAdres' ");
-
-} else {
-  echo 'shit';}
-}
-
-    
-
-
-
-
-
-
-?>
+                    } else {
+                        echo 'foutmelding';
+                    }
+                }
+            ?>
+    </form>
 </main>
 <?php require_once 'footer.php'; ?>
