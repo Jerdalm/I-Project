75140f306f7ded56210ff27f626b6a31ff6269eb
Kev4ever <kevin.van.schaijk01@gmail.com>
2018-05-28 12:14:30 +0200

    seller registration

--- layout/seller-registration.php
+++ layout/seller-registration.php
@@ -1,5 +1,7 @@
-<div class="tab-pane fade" id="content-seller-registration" role="tabpanel" aria-labelledby="seller-registration">
-	<div class="tab-pane fade show active" id="content-user-details" role="tabpanel" aria-labelledby="list-user-details">
+<div class="tab-pane fade float-left col-lg-9 " id="content-seller-registration" role="tabpanel" aria-labelledby="seller-registration">
 		<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 text-center"><h3>Registreren als verkoper</h3></div>
+		<div class="row justify-content-md-center">
+		<a type="button" class="cta-orange" href="upgrade-user.php">wordt verkoper!</a>
+		</div>
 	</div>
 </div>
