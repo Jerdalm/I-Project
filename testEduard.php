d78316de9a23375f5f231889a8f46e9b2fa6136a
eduardbakouev <edi_bakouev@hotmail.nl>
2018-05-29 15:34:47 +0200

    email naar gesloten veilingen

--- testEduard.php
+++ testEduard.php
@@ -2,17 +2,16 @@
 
 $geslotenVeilingen = handlequery("SELECT * FROM voorwerp v join gebruiker G on v.verkoper = g.gebruikersnaam WHERE looptijdEindeDag <= CONVERT(date, GETDATE()) 
 								 AND V.veilingGesloten = 0 AND looptijdEindeTijdstip < CONVERT(time, GETDATE());");
-
+// hier worden de mailtjes gestuurd voor de veilingen die nog op 0 staan en afgelopen zijn
 if (isset($_POST['sendmail'])){
 
 		foreach($geslotenVeilingen as $gesloten){
 		
 		$geslotenParameters = array(':voorwerpnummer' => $gesloten['voorwerpnummer']);
 		$subject = 'veiling:' . $gesloten['titel'] . 'is gesloten';
-		$message = 'De veiling' .' '. $gesloten['titel'] .' '. 'is Gesloten. De winnar van deze veiling is:' .' '. $gesloten['koper'];
+		$message = 'De veiling' .' '. $gesloten['titel'] .' '. 'is Gesloten. De winnaar van deze veiling is:' .' '. $gesloten['koper'];
 		$email = $gesloten['mailadres'];
 		sendMail($email,$subject,$message);
-		handlequery("UPDATE voorwerp SET veilingGesloten = 1 WHERE voorwerpnummer = :voorwerpnummer",$geslotenParameters);
 			
 		}
 	}
@@ -20,7 +19,7 @@ if (isset($_POST['sendmail'])){
 
 <main class="geslotenVeilingen">
 	<section>
-		 <i class="fas fa-plus-circle"></i>
+
 		<form class="form-group" method="POST">
 			<input type="submit" class="cta-orange btn" name="sendmail" value="Check veilingen">
 		</form>
