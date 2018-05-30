ea44826d7f8723abff7066a90650363bac8bab1e
eduardbakouev <edi_bakouev@hotmail.nl>
2018-05-30 15:37:59 +0200

    mail functie

--- testEduard.php
+++ testEduard.php
@@ -1,21 +1,44 @@
 <?php require_once 'header.php'; 
 
-$geslotenVeilingen = handlequery("SELECT * FROM voorwerp v join gebruiker G on v.verkoper = g.gebruikersnaam WHERE looptijdEindeDag <= CONVERT(date, GETDATE()) 
-								 AND V.veilingGesloten = 0 AND looptijdEindeTijdstip < CONVERT(time, GETDATE());");
+
 // hier worden de mailtjes gestuurd voor de veilingen die nog op 0 staan en afgelopen zijn
 if (isset($_POST['sendmail'])){
+		sendEmailClosedAuctions();
 
-		foreach($geslotenVeilingen as $gesloten){
 		
-		$geslotenParameters = array(':voorwerpnummer' => $gesloten['voorwerpnummer']);
-		$subject = 'veiling:' . $gesloten['titel'] . 'is gesloten';
-		$message = 'De veiling' .' '. $gesloten['titel'] .' '. 'is Gesloten. De winnaar van deze veiling is:' .' '. $gesloten['koper'];
+	}
+
+function getUserData($user){
+	$return = handlequery("SELECT * FROM voorwerp v join gebruiker G on v.".$user." = g.gebruikersnaam WHERE looptijdEindeDag <= CONVERT(date, GETDATE()) 
+								 		  AND V.veilingGesloten = 0 AND looptijdEindeTijdstip < CONVERT(time, GETDATE());");
+	return $return;
+}
+
+
+function sendEmailClosedAuctions(){
+	foreach(getUserData("verkoper") as $gesloten){
 		$email = $gesloten['mailadres'];
+		$subject = 'veiling:' . $gesloten['titel'] . 'is gesloten';
+		if(!empty ($gesloten['koper'])) {
+			$message = 'De veiling' .' '. $gesloten['titel'] .' '. 'is Gesloten. de veiling is gewonnen door:' . $gesloten['koper'] . '.Bekijk de veiling op: http://localhost/EenmaalAndermaal/GitHub/I-Project/productpage.php?product=' . $gesloten['voorwerpnummer'];
+		} else {
+			$message = 'De veiling' .' '. $gesloten['titel'] .' '. 'is Gesloten. Uw veiling heeft jammer genoeg geen koper. Bekijk de veiling op: http://localhost/EenmaalAndermaal/GitHub/I-Project/productpage.php?product=' . $gesloten['voorwerpnummer'];
+		}
 		sendMail($email,$subject,$message);
+	}
 			
+	foreach(getUserData("koper") as $gesloten){
+		if (!empty($gesloten['koper'])){
+			$subject = 'Gefeliciteerd u heeft veiling:' . $gesloten['titel'] . ' ' . 'Gewonnen!';
+			$message = 'De veiling' .' '. $gesloten['titel'] .' '. 'is Gesloten. Gefeliciteerd! U bent de winnaar van deze Veiling. Bekijk de veiling op: http://localhost/EenmaalAndermaal/GitHub/I-Project/productpage.php?product=' . $gesloten['voorwerpnummer'];
+			$email = $gesloten['mailadres'];
+			sendMail($email,$subject,$message);
 		}
 	}
+}
 ?>
+
+
 
 <main class="geslotenVeilingen">
 	<section>
