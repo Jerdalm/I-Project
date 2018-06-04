e92dc99ce4bfb9a12cf2e0fd79e0557177389f68
TLintsen <38420486+TLintsen@users.noreply.github.com>
2018-06-04 10:37:32 +0200

    a

---
+++ aboutus.php
@@ -0,0 +1,14 @@
+<?php require_once 'header.php';?>
+<main>
+  <section class ='aboutus'>
+    Wij zijn een team van jonge developers die met veel passie aan deze site gewerkt hebben!
+    <ul>
+      <li>Bart</li>
+      <li>Eduard</li>
+      <li>Jeremy</li>
+      <li>Jonathan</li>
+      <li>Timo</li>
+  </section>
+</main>
+
+<?php require_once 'footer.php';?>
--- footer.php
+++ footer.php
@@ -3,18 +3,18 @@
       <div class="container">
        <div class="row ">
 		 <div class="col-lg-5  col-md-8 col-sm-12 col-xs-12 fContent text-center align-center">
-		 
+
 		 <h2><b>Eenmaal</b></h2>
 		 <img class="logo" src="img/logo/logo.svg">
 		 <h2> andermaal</h2>
-		 
+
 		</div>
 		<div class="col-lg-1 col-lg-1"></div>
 		<div class="col-lg-2  col-md-4 col-sm-6 col-xs-12 mx-auto fContent separationline">
 		<h6>Over ons</h6>
 		<hr>
 			<ul>
-				<li><a href="#">Leer ons kennen</a></li>
+				<li><a href="aboutus.php">Leer ons kennen</a></li>
 				<li><a href="#">Sitemap</a></li>
 				<li><a href="#">Disclaimer</a></li>
 			</ul>
@@ -22,7 +22,7 @@
 
 		<div class="col-lg-3 col-md-12 col-sm-6 col-xs-12 socialdiv fContent text-center">
 		  <h6>Onze socials</h6>
-		  
+
 		  <div class="social">
 		  <a class="rounded-circle" href="#">
 		  <i class="fab fa-facebook-f"></i>
@@ -37,19 +37,17 @@
 		</div>
 		<div class="col-lg-1"></div>
 	</div>
-  
+
       </div>
       <!-- /.container -->
     </footer>
 	<!-- Veilingen draaien -->
 
-	
-	
+
+
 
     <!-- Bootstrap core JavaScript -->
     <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-	  
+
   </body>
 </html>
-
-
