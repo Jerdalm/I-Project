6e8dd5e3a036923006362a0a67a1ce1468e19122
Kev4ever <kevin.van.schaijk01@gmail.com>
2018-06-04 11:42:01 +0200

    al weg 404

--- 404.php
+++ 404.php
@@ -7,7 +7,7 @@
          <div class="col-lg-12 " >
 		  <h1 class="display-3 text-center text-white"> <b>Eenmaal</b> andermaal</h1>
             <p class="text-center text-white">
-			O sorry, wij zien hier al aan het opruimen! <br>
+			O sorry, wij zien hier aan het opruimen! <br>
 			Klik op de onderstaande button om terug te gaan.
 			</p>
 			<a href="index.php" class="btn btn-orange">Terug naar de homepage</a>
