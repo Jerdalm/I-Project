e9d082cc636d52c4d1490e29dca8a559ccdd2f47
TLintsen <38420486+TLintsen@users.noreply.github.com>
2018-06-06 10:31:19 +0200

    HOOFDLETTERS GODVERDOMME

--- admin/footer.php
+++ admin/footer.php
@@ -3,11 +3,11 @@
     	<div class="container">
     		<div class="row ">
     			<div class="col-lg-5  col-md-8 col-sm-12 col-xs-12 fContent text-center align-center">
-    				
+
     				<h2><b>Eenmaal</b></h2>
     				<img class="logo" src="img/logo/logo.svg">
-    				<h2> andermaal</h2>
-    				
+    				<h2> Andermaal</h2>
+
     			</div>
     			<div class="col-lg-1 col-lg-1"></div>
     			<div class="col-lg-2  col-md-4 col-sm-6 col-xs-12 mx-auto fContent separationline">
@@ -22,7 +22,7 @@
 
     			<div class="col-lg-3 col-md-12 col-sm-6 col-xs-12 socialdiv fContent text-center">
     				<h6>Onze socials</h6>
-    				
+
     				<div class="social">
     					<a class="rounded-circle" href="#">
     						<i class="fab fa-facebook-f"></i>
@@ -37,7 +37,7 @@
     			</div>
     			<div class="col-lg-1"></div>
     		</div>
-    		
+
     	</div>
     	<!-- /.container -->
     </footer>
@@ -48,5 +48,3 @@
     <script src="vendor/bootstrap/js/popup.header.js"></script>
 </body>
 </html>
-
-
