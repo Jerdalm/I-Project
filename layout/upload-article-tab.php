45839dec3fa1da1512fe694c729d83339f5b4752
TLintsen <38420486+TLintsen@users.noreply.github.com>
2018-06-06 13:25:33 +0200

    5

---
+++ layout/upload-article-tab.php
@@ -0,0 +1,8 @@
+<div class="tab-pane fade float-left col-lg-9 " id="content-upload-article" role="tabpanel" aria-labelledby="upload-article">
+		<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 text-center"><h3>Registreren als verkoper</h3></div>
+		<div class="row justify-content-md-center">
+		<a type="button" class="cta-orange" href="upgrade-user.php">wordt verkoper!</a>
+		</div>
+	</div>
+</div>
+
