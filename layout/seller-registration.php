3f52a721bd1926952f9e793c4eff953be7dc4da9
TLintsen <38420486+TLintsen@users.noreply.github.com>
2018-06-07 14:32:28 +0200

    a

--- layout/seller-registration.php
+++ layout/seller-registration.php
@@ -1,7 +1,7 @@
 <div class="tab-pane fade float-left col-lg-9 " id="content-seller-registration" role="tabpanel" aria-labelledby="seller-registration">
 		<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 text-center"><h3>Registreren als verkoper</h3></div>
 		<div class="row justify-content-md-center">
-		<a type="button" class="cta-orange" href="upgrade-user.php">wordt verkoper!</a>
+		<a type="button" class="cta-orange" href="upgrade-user.php">Word verkoper!</a>
 		</div>
 	</div>
 </div>
