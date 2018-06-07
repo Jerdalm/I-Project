98a6a5b1b35f81ad6776a52c83be8f73c218d7b3
eduardbakouev <edi_bakouev@hotmail.nl>
2018-06-07 15:44:22 +0200

    info passmodal en qachtwoord vergeten

--- account.php
+++ account.php
@@ -1,54 +1,50 @@
 <?php
 require_once 'header.php';
 if(isset($_SESSION['gebruikersnaam'])){
-?>
-<header id="account" class="header content-header">
+  ?>
+  <header id="account" class="header content-header">
    <div class="container">
-      <div class="row text-center">
-         <div class="col-lg-12 " >
-            <h1 class="display-3 text-center text-white">Account</h1>
-         </div>
-      </div>
-   </div>
+    <div class="row text-center">
+     <div class="col-lg-12 " >
+      <h1 class="display-3 text-center text-white">Account</h1>
+    </div>
+  </div>
+</div>
 </header>
 <section class="user-details">
-    <div class="container">
-	<div class="col-lg-3 sidebar float-left">
+  <div class="container">
+   <div class="col-lg-3 sidebar float-left">
     <div class="list-group" id="list-tab" role="tablist">
+      <a class="list-group-item list-group-item-action active" id="list-user-details" data-toggle="list" href="#content-user-details" role="tab" aria-controls="user-details">Gebruikersgegevens</a>
+      <a class="list-group-item list-group-item-action" id="list-offering-in"    data-toggle="list" href="#content-offering-in" role="tab" aria-controls="offering-in">Geboden op</a>
+      <a class="list-group-item list-group-item-action" id="list-auctions-won"   data-toggle="list" href="#content-auctions-won" role="tab" aria-controls="auctions-won">Gewonnen veilingen</a>
 
-	
-  <a class="list-group-item list-group-item-action active" id="list-user-details" data-toggle="list" href="#content-user-details" role="tab" aria-controls="user-details">Gebruikersgegevens</a>
-  
-    
-        <?php
-        if($_SESSION['soortGebruiker'] > 1){ ?>
-    <a class="list-group-item list-group-item-action" id="list-my-auctions" data-toggle="list" href="#content-my-auctions" role="tab" aria-controls="my-auctions">Mijn veilingen</a>
-    <a class="list-group-item list-group-item-action" id="list-upload-article" data-toggle="list" href="#content-upload-article" role="tab" aria-controls="upload-article">Verkoop voorwerp</a>
-        <?php } ?>
-    <a class="list-group-item list-group-item-action" id="list-offering-in" data-toggle="list" href="#content-offering-in" role="tab" aria-controls="offering-in">Geboden op</a>
-    <a class="list-group-item list-group-item-action" id="list-auctions-won" data-toggle="list" href="#content-auctions-won" role="tab" aria-controls="auctions-won">Gewonnen veilingen</a>
+      <?php
+      if($_SESSION['soortGebruiker'] > 1){ ?>
+      <a class="list-group-item list-group-item-action" id="list-my-auctions"    data-toggle="list" href="#content-my-auctions" role="tab" aria-controls="my-auctions">Mijn veilingen</a>
+      <a class="list-group-item list-group-item-action" id="list-upload-article" data-toggle="list" href="#content-upload-article" role="tab" aria-controls="upload-article">Verkoop voorwerp</a>
+      <?php } ?>
 
-
-
-	<?php
-	if($_SESSION['soortGebruiker'] < 2){ ?>
-	<a class="list-group-item list-group-item-action" id="list-seller-registration" data-toggle="list" href="#content-seller-registration" role="tab" aria-controls="seller-registration">Verkoper registratie</a>
-	<?php } ?>
+      <?php
+      if($_SESSION['soortGebruiker'] < 2){ ?>
+      <a class="list-group-item list-group-item-action" id="list-seller-registration" data-toggle="list" href="#content-seller-registration" role="tab" aria-controls="seller-registration">Verkoper registratie</a>
+      <?php } ?>
+      
     </div>
-</div>
-	<div class="tab-content" id="nav-tabContent">
-     <?php
+  </div>
+  <div class="tab-content" id="nav-tabContent">
+   <?php
    require 'layout/user-details.php';
    require 'layout/auctionswon.php';
    require 'layout/upload-article-tab.php';
-     require 'layout/my-auctions.php';
-     require 'layout/offering-in.php';
+   require 'layout/my-auctions.php';
+   require 'layout/offering-in.php';
    require 'layout/seller-registration.php';
 
-	 ?>
-    </div>
-	<div class="clearfix"></div>
-   </div>
+   ?>
+ </div>
+ <div class="clearfix"></div>
+</div>
 </section>
 
 <?php
--- layout/changeinfoModal.php
+++ layout/changeinfoModal.php
@@ -9,79 +9,79 @@
         </button>
       </div>
       <div class="modal-body">
-      <!-- echo om alle data in een formulier te zetten en te zorgen dat de gegevens aangepast kunnen worden. -->
-            <form method="get" class="form-group col-lg-12 edit-user-info">  
-                <?php  foreach ($gebruiker as $key => $value) { 
-                    echo '<label><b>'.$key.'</b></label>';
-                    switch ($key) {
-                        case 'geboortedag':
-                        echo '<input class="form-control" type="date" name="' . $key . '" value="'. $value .'" required><br>';
-                        break;
-                        case 'gebruikersnaam':
-                        echo '<input class="form-control" type="text" name="' . $key . '" value="'. $value .'" readonly><br>';
-                        break;
-                        case 'telefoonnummer':
-                      
-                        break;
-                        case 'adresregel2':
-                        echo '<input class="form-control" type="text" name="' . $key . '" value="'. $value .'"><br>';
-                        break;
-                        default:
-                        echo '<input class="form-control" type="text" name="' . $key . '" value="'. $value .'" required><br>';
-                        break;
-                    }
-                } 
-                ?>
-                <div class="input_fields_wrap">
-                <?php
-                $increment = 0;
-                        foreach($telefoonnummers as $nummer){
-                         echo '
-                              <div><input value="'.$nummer[0]. '" class="form-control" type="text" name="telefoonnummer'.$increment .'"></div>
-                              
-                              ';
-                              $increment++;
-                        }
-                ?>
-                </div>              
-                <button class="add_field_button btn btn-orange">Add More Fields</button>
-                    
+        <!-- echo om alle data in een formulier te zetten en te zorgen dat de gegevens aangepast kunnen worden. -->
+        <form method="get" class="form-group col-lg-12 edit-user-info">  
+          <?php  foreach ($gebruiker as $key => $value) { 
+            echo '<label><b>'.$key.'</b></label>';
+            switch ($key) {
+              case 'geboortedag':
+              echo '<input class="form-control" type="date" name="' . $key . '" value="'. $value .'" required><br>';
+              break;
+              case 'gebruikersnaam':
+              echo '<input class="form-control" type="text" name="' . $key . '" value="'. $value .'" readonly><br>';
+              break;
+              case 'telefoonnummer':
+              
+              break;
+              case 'adresregel2':
+              echo '<input class="form-control" type="text" name="' . $key . '" value="'. $value .'"><br>';
+              break;
+              default:
+              echo '<input class="form-control" type="text" name="' . $key . '" value="'. $value .'" required><br>';
+              break;
+            }
+          } 
+          ?>
+          <div class="input_fields_wrap">
+            <?php
+            $increment = 0;
+            foreach($telefoonnummers as $nummer){
+             echo '
+             <div><input value="'.$nummer[0]. '" class="form-control" type="text" name="telefoonnummer'.$increment .'"></div>
+             
+             ';
+             $increment++;
+           }
+           ?>
+         </div>              
+         <button class="add_field_button btn btn-orange">Add More Fields</button>
+         
 
-      </div>
-      <div class="modal-footer">
+       </div>
+       <div class="modal-footer">
 
         <button type="button" class="btn btn-secondary" data-dismiss="modal">Annuleren</button>
         <button type="submit" class="btn btn-primary" name="bijwerken">Bijwerken</button>
       </div>
-	  </form>
-	</form>
-    </div>
-  </div>
+    </form>
+  </form>
+</div>
+</div>
 </div>
 <script>
 
-$(document).ready(function() {
+  $(document).ready(function() {
     var max_fields      = 4 - <?= $aantalTelefoonNummers ?>; //maximum input boxes allowed
     var wrapper         = $(".input_fields_wrap"); //Fields wrapper
     var add_button      = $(".add_field_button"); //Add button ID
     var counter         = <?= $increment ?>;
-   
+    
     var x = 1; //initlal text box count
     $(add_button).click(function(e){ //on add input button click
-        e.preventDefault();
+      e.preventDefault();
         if(x < max_fields){ //max input box allowed
             x++; //text box increment
             $(wrapper).append('<div class="input-group"><input type="text" class=form-control name="telefoonnummer'+ counter +
 			'"/><div class="input-group-append"><a href="#" class=" remove_field btn btn-danger"><i class="fas fa-times"></i></a> </div></div>'); //add input box
             counter++;
-        }
-    });
-   
+          }
+        });
+    
     $(wrapper).on("click",".remove_field", function(e){ //user click on remove text
-        e.preventDefault(); $(this).parent('div').parent('div').remove(); x--;
-        counter--;
+      e.preventDefault(); $(this).parent('div').parent('div').remove(); x--;
+      counter--;
     })
-});
+  });
 
 </script>
 
